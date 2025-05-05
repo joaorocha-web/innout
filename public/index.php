3686f26b4309ca41f860528fdc2106e109482beb
joaorocha-web <jvrochajf@eaportal.org>
2025-05-05 17:56:45 -0300

    Criando  metodos na classe database

--- public/index.php
+++ public/index.php
@@ -1,1 +1,12 @@
-<h1>Okay</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+$sql = "SELECT * FROM users";
+$result= Database::getResultFromQuery($sql);
+
+while($row = $result->fetch_assoc()){
+    print_r($row);
+    echo "<br>";
+}
+?>
