722fc3d5a3fb26f627f6995155627806237d2805
joaorocha-web <jvrochajf@eaportal.org>
2025-05-06 13:38:31 -0300

    .

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/user.php');
 
 $user = new User(['name' => 'João Lindão', 'email' => 'ola@email.com']);
-print_r($user->email);
+echo $user->getSelect('id, name')
 
 
 // $user->set('email' , 'jvrocha@ola.com');
--- src/models/model.php
+++ src/models/model.php
@@ -6,27 +6,27 @@ class Model{
     protected $values = [];
 
     function __construct($arr){
-        $this -> loadFromArr($arr);
+        $this->loadFromArr($arr);
     } 
 
     public function loadFromArr($arr){
         if ($arr){
             foreach($arr as $key => $value){
-                $this ->$key= $value;
+                $this->$key= $value;
             }
         }
     }
 
     public function __get($key){
-        return $this -> values[$key]; 
+        return $this->values[$key]; 
     }
 
     public function __set($key, $value){
-        $this -> values[$key] = $value;
+        $this->values[$key] = $value;
     }
 
-    public function getSelect(){
-        $sql = 'SELECT * FROM ' . static::$tableName;
+    public function getSelect($columns = '*'){
+        $sql = "SELECT $columns FROM " . static::$tableName;
         return $sql;
     }
 }
