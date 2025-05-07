7164390a86fec8aca6aa4a1e35bba7a01b522db9
joaorocha-web <jvrochajf@eaportal.org>
2025-05-07 14:43:46 -0300

    pagina de login

--- src/views/login.php
+++ src/views/login.php
@@ -5,18 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/comum.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/icofont.mim.css">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css?v=9999">
     <title>In 'n Out</title>
 </head>
 <body>
-    <form action="#" method="post">
+    <form class="form-login" action="#" method="post">
         <div class="login-card card">
             <div class="card-header">
-                
+                <i class="icofont-travelling mr-2"></i>
+                <span class="font-weight-light">In</span>
+                <span class="font-weight-bold mx-1">'N</span>
+                <span class="font-weight-light">Out</span>
+                <i class="icofont-runner-alt-1 ml-2"></i>
             </div>
             <div class="card-body">
-
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" id="email" name="email" class="form-control" placeholder="Informe o e-mail" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" class="form-control" placeholder="Informe a senha" >
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-md btn-primary">Entrar</button>
             </div>
         </div>
     </form>
