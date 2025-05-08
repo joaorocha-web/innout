951e201419e07721b9f15a2d8f6dfaab5727005f
joaorocha-web <jvrochajf@eaportal.org>
2025-05-08 16:22:25 -0300

    efeito hover aplicado sobre o botao

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -24,14 +24,14 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span>Usuário</span>
-                <i class="icofont-simple-down ml-2"></i>
+                <span class="ml-3">Usuário</span>
+                <i class="icofont-simple-down mx-2"></i>
             </div>
             <div class="dropdown-content">
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php">
-                            <i class="icofont-logout mr-2"></i>
+                            <i class="icofont-logout mr-2"></i>Sair
                         </a>
                     </li>
                 </ul>
