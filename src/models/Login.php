bcb5547f93fb193afff476f29d3940117b9eed9f
joaorocha-web <jvrochajf@eaportal.org>
2025-05-08 10:17:42 -0300

    .

--- src/models/Login.php
+++ src/models/Login.php
@@ -6,10 +6,13 @@ class Login extends Model {
     public function checkLogin(){
         $user = User::getOne(['email' => $this->email]);
         if ($user){
+            if($user->end_date){
+                throw new AppException('Usuário desligado da empresa');   
+            }
             if(password_verify($this->password, $user->password)){
                 return $user;
             }
         }
-        throw new Exception();
+        throw new AppException('Usuário/Senha inválidos');
     }
 }
