4b8654e32611d698a6c9ce7ee1b8f48202656452
joaorocha-web <jvrochajf@eaportal.org>
2025-05-09 12:15:46 -0300

    css footer

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,5 @@
 <footer class="footer">
-    <span>Desenvolvido com </span>
-    <span class="icofont-heart text-danger mx-1"></span>
-    <span>por COD3R</span>
+    <span>Desenvolvido por COD3R</span>
 
 </footer>
 <script src="assets/js/app.js"></script>
