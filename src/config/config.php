9e18d5564132f4e4c1f5e96ddc329f98611ff7b4
joaorocha-web <jvrochajf@eaportal.org>
2025-05-12 14:04:21 -0300

    Gerando massa de dados

--- src/config/config.php
+++ src/config/config.php
@@ -2,6 +2,9 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt-BR', 'pt-BR.utf-8', 'portuguese');
 
+//GERAIS
+define('DAILY_TIME', 60 * 60 * 8);
+
 //PASTAS
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
