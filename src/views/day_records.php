7b63cd4fa0eb9dcc2680d30e1b81ceeae4c74388
joaorocha-web <jvrochajf@eaportal.org>
2025-05-12 11:52:57 -0300

    .

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,12 +1,16 @@
 <main class="content">
-    <?php renderTitle(
+    <?php 
+    renderTitle(
         'Registrar Ponto',
         'Mantenha seu ponto consistente!',
         'icofont-check-alt'
-    ) ?>
+    );
+
+    include(TEMPLATE_PATH . "/messages.php");
+     ?>
    <div class="card">
     <div class="card-header">
-        <h3>10 de janeiro de 2025</h3>
+        <h3><?=$today?></h3>
         <p class="mb-0">Os batimentos efetuados hoje</p>
     </div>
     <div class="card-body">
