0165dd9667f46511e7b6787482506776b930d4e8
joaorocha-web <jvrochajf@eaportal.org>
2025-05-15 12:11:18 -0300

    interação do time com javascript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,3 +8,30 @@
 }
 })()
 
+
+
+function activateClock(){
+    const activeClock = document.querySelector('[active-clock]')
+    if(!activeClock) return 
+
+    function addOneSecond(hours, minutes, seconds){
+    const data = new Date()
+    data.setHours(parseInt(hours))
+    data.setMinutes(parseInt(minutes))
+    data.setSeconds(parseInt(seconds) + 1)
+
+    const h = `${data.getHours()}`.padStart(2, 0)
+    const m = `${data.getMinutes()}`.padStart(2, 0)
+    const s = `${data.getSeconds()}`.padStart(2, 0)
+
+    return `${h}:${m}:${s}`
+
+    }
+
+    setInterval(function (){
+        const parts = activeClock.innerHTML.split(':')
+        activeClock.innerHTML = addOneSecond(...parts)
+    }, 1000)
+}
+
+activateClock();
