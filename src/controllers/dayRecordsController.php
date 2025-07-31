7be277ff38b1728b8972bcd4e66ff281f626ebc4
joaorocha-web <jvrochajf@eaportal.org>
2025-07-31 18:36:34 -0300

    Método update para o batimento de ponto

--- src/controllers/dayRecordsController.php
+++ src/controllers/dayRecordsController.php
@@ -2,9 +2,15 @@
 session_start();
 requireValidSession();
 
+loadModel('WorkingHours');
+
 $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date);
-loadTemplateView('day_records', ['today' => $today]);
+
+$user = $_SESSION['user'];
+$userWorkingHours = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
+
+loadTemplateView('day_records', ['today' => $today, 'records' => $userWorkingHours]);
 
 
 
